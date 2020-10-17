652d8e9468b412a9ab3b33301eeb5151203de480
Denis Serdyuk <diserdyuk@gmail.com>
2020-10-17 10:28:29 +0300

    Get html code wordpress page

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_html(url):    # фунция отсылает запрос на сервер и получ. ответ
+    r = requests.get(url)   # получаю rеsponsе (ответ)
+    return r.text
+
+
+def main():
+    url = 'https://wordpress.org/'
+    print(get_html(url))
 
 
 
 
 
 
-
-
-
-
-
-if __name__ == "__main__":   # точка входа
+if __name__ == "__main__":    # точка входа
     main()
-
